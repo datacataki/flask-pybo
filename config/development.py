db24c0ff3c0d16fb7e401edc4badff5d5a89c304
y2gon <y2gon2@naver.com>
2021-11-12 17:34:19 +0900

    adding previous executable package

--- config/development.py
+++
@@ -1,5 +0,0 @@
-from config.default import *
-
-SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'android_login.db'))
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-SECRET_KEY = "dev"
